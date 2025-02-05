b6464f8db4b7dc156f3b8e35ae22c07afd22ca5e
Simon Greaves <35945230+segreaves@users.noreply.github.com>
2025-02-05 15:43:00 -0500

    MoveZeroes
    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
    
    Note that you must do this in-place without making a copy of the array.
    
     
    
    Example 1:
    
    Input: nums = [0,1,0,3,12]
    Output: [1,3,12,0,0]
    Example 2:
    
    Input: nums = [0]
    Output: [0]

---
+++ MoveZeroes.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+void moveZeroes(std::vector<int>& arr)
+{
+    int l{ 0 };
+    int r{ static_cast<int>(arr.size()) };
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > 0) arr[l++] = arr[i];
+        else r--;
+    }
+    for (int j = r; j < arr.size(); j++)
+        arr[j] = 0;
+}
+
+void print(std::vector<int>& arr)
+{
+    for (const auto& elem : arr)
+        std::cout << elem << " ";
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::cout << "Should be 1 3 12 0 0: " << std::endl;
+    std::vector<int> ex1{0, 1, 0, 3, 12};
+    moveZeroes(ex1);
+    print(ex1);
+    std::cout << "Should be 0: " << std::endl;
+    std::vector<int> ex2{0};
+    moveZeroes(ex2);
+    print(ex2);
+}
