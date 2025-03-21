acd757193c2a43588cb87ff6e566a288d0389407
Simon Greaves <35945230+segreaves@users.noreply.github.com>
2025-03-21 09:45:09 -0400

    Linked List Cycle
    Given head, the head of a linked list, determine if the linked list has a cycle in it.
    There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
    Return true if there is a cycle in the linked list. Otherwise, return false.

---
+++ LinkedListCycle.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode *head)
+{
+    ListNode* tortoise = head;
+    ListNode* hare = head;
+    while (hare && hare->next)
+    {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise) return true;
+    }
+    return false;
+}
+
+int main()
+{
+    ListNode* list1_1{ new ListNode(3) };
+    ListNode* list1_2{ new ListNode(2) };
+    list1_1->next = list1_2;
+    ListNode* list1_3{ new ListNode(0) };
+    list1_2->next = list1_3;
+    ListNode* list1_4{ new ListNode(-4) };
+    list1_3->next = list1_4;
+    list1_4->next = list1_2;
+    std::cout << "Should be true: " << hasCycle(list1_1) << std::endl;
+
+    ListNode* list2_1{ new ListNode(1) };
+    ListNode* list2_2{ new ListNode(2) };
+    list2_1->next = list2_2;
+    list2_2->next = list2_1;
+    std::cout << "Should be true: " << hasCycle(list2_1) << std::endl;
+
+    ListNode* list3_1{ new ListNode(1) };
+    std::cout << "Should be false: " << hasCycle(list3_1) << std::endl;
+}
