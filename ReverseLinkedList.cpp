5a2c854db84e4290a9ffd07184c56ed8dfab1dac
Simon Greaves <35945230+segreaves@users.noreply.github.com>
2025-03-24 09:56:27 -0400

    Reverse Linked List
    Given the head of a singly linked list, reverse the list, and return the reversed list.
    
    Example 1:
    Input: head = [1,2,3,4,5]
    Output: [5,4,3,2,1]
    
    Example 2:
    Input: head = [1,2]
    Output: [2,1]
    
    Example 3:
    Input: head = []
    Output: []

---
+++ ReverseLinkedList.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <list>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode* reverseList(ListNode* head)
+{
+    ListNode* prev = nullptr;
+    while (head)
+    {
+        ListNode* next{ head->next };
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
+
+void printList(ListNode* head)
+{
+    ListNode* curr{ head };
+    while (curr)
+    {
+        std::cout << curr->val << " ";
+        curr = curr->next;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    ListNode* l1_5{ new ListNode(5) };
+    ListNode* l1_4{ new ListNode(4, l1_5) };
+    ListNode* l1_3{ new ListNode(3, l1_4) };
+    ListNode* l1_2{ new ListNode(2, l1_3) };
+    ListNode* l1_1{ new ListNode(1, l1_2) };
+    std::cout << "l1: ";
+    printList(l1_1);
+    ListNode* l1_rev{ reverseList(l1_1) };
+    std::cout << "Reversed l1: ";
+    printList(l1_rev);
+
+    ListNode* l2_2{ new ListNode(2) };
+    ListNode* l2_1{ new ListNode(1, l2_2) };
+    std::cout << "l2: ";
+    printList(l2_1);
+    ListNode* l2_rev{ reverseList(l2_1) };
+    std::cout << "Reversed l2: ";
+    printList(l2_rev);
+
+    ListNode* l3;
+    std::cout << "l3: ";
+    printList(l3);
+    ListNode* l3_rev{ reverseList(l3) };
+    std::cout << "Reversed l3: ";
+    printList(l3_rev);
+}
