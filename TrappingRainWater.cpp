22a3569a9be7ab3c8992c4613ba12b050e6532ac
Simon Greaves <35945230+segreaves@users.noreply.github.com>
2025-07-18 09:47:48 -0400

    Update TrappingRainWater.cpp
    - Added use of auto and const.
    - Other small optimizations for speed and readability.

--- TrappingRainWater.cpp
+++ TrappingRainWater.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <vector>
 
-int trap(std::vector<int>& height)
+int trap(const std::vector<int>& height)
 {
-    int n{ static_cast<int>(height.size()) };
-    if (n <= 2) return 0;
-    std::vector<int> max_l(n, 0);
-    max_l[0] = height[0];
-    std::vector<int> max_r(n, 0);
-    max_r[n - 1] = height[n - 1];
-    for (int i = 1; i < n; i++)
+    const auto n{ height.size() };
+    std::vector<int> max_l(n);
+    std::vector<int> max_r(n);
+    auto l{ 0 };
+    auto r{ 0 };
+    for (size_t i = 0; i < n; ++i)
     {
-        max_l[i] = std::max(max_l[i - 1], height[i]);
-        max_r[n - i - 1] = std::max(max_r[n - i], height[n - i - 1]);
+        l = std::max(l, height[i]);
+        r = std::max(r, height[n - 1 - i]);
+        max_l[i] = l;
+        max_r[n - 1 - i] = r;
     }
-    int w{ 0 };
-    for (int i = 1; i < n - 1; i++)
+    auto water{ 0 };
+    for (size_t i = 1; i < n - 1; ++i)
     {
-        int lowest_max_height{ std::min(max_l[i - 1], max_r[i + 1]) };
-        int curr_height{ height[i] };
-        w += (lowest_max_height > curr_height ? lowest_max_height - curr_height : 0);
+        const auto height_l{ max_l[i] };
+        const auto height_r{ max_r[i] };
+        const auto min_height{ std::min(height_l, height_r) };
+        water += std::max(0, min_height - height[i]);
     }
-    return w;
+    return water;
 }
 
 int main()
 {
-    std::vector<int> ex_1{ 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+    const std::vector<int> ex_1{ 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
     std::cout << "Should be 6: " << trap(ex_1) << std::endl;
 
-    std::vector<int> ex_2{ 4, 2, 0, 3, 2, 5 };
+    const std::vector<int> ex_2{ 4, 2, 0, 3, 2, 5 };
     std::cout << "Should be 9: " << trap(ex_2) << std::endl;
 }
