ccdca529517e8d2f5948c67cf7e280dbd28cab37
Simon Greaves <35945230+segreaves@users.noreply.github.com>
2025-09-11 09:59:55 -0400

    Majority Element
    Given an array nums of size n, return the majority element.
    The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
    
    Example 1:
    Input: nums = [3,2,3]
    
    Output: 3
    Example 2:
    Input: nums = [2,2,1,1,1,2,2]
    Output: 2

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 
 int majorityElement(std::vector<int>& nums)
 {
-    std::unordered_map<int, int> m;
-    for (const auto& i : nums)
+    int n{ static_cast<int>(nums.size()) };
+    int majority_element{ nums[0] };
+    int count{ 1 };
+    for (int i = 1; i < n; ++i)
     {
-        m[i]++;
-        if (m[i] > nums.size() / 2) return i;
+        if (nums[i] == majority_element) ++count;
+        else if (--count == 0)
+        {
+            majority_element = nums[i];
+            count = 1;
+        }
     }
-    return -1;
+    return majority_element;
 }
 
 int main()
 {
-    std::vector<int> ex1{ 3, 2, 3 };
-    std::cout << "Should be 3: " << majorityElement(ex1) << std::endl;
+    std::vector<int> arr1{ 3, 2, 3 };
+    std::cout << "Should be 3: " << majorityElement(arr1) << "\n";
 
-    std::vector<int> ex2{ 2, 2, 1, 1, 1, 2, 2 };
-    std::cout << "Should be 2: " << majorityElement(ex2) << std::endl;
+    std::vector<int> arr2{ 2, 2, 1, 1, 1, 2, 2 };
+    std::cout << "Should be 2: " << majorityElement(arr2) << "\n";
 }
