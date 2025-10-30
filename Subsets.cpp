6c9f8646ce684d05907d59120ec81b72af992f1e
Simon Greaves <35945230+segreaves@users.noreply.github.com>
2025-10-30 10:28:38 -0400

    Small improvement to Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -2,42 +2,18 @@
 #include <vector>
 #include <cmath>
 
-std::vector<std::vector<int>> subsets1(std::vector<int>& nums)
+std::vector<std::vector<int>> subsets(std::vector<int>& nums)
 {
     std::vector<std::vector<int>> ans;
-    ans.reserve(pow(2, nums.size()));
+    // Number of subsets is nums.size() squared
+    ans.reserve(std::pow(2, nums.size()));
     ans.emplace_back(std::vector<int>());
     for (const auto& e : nums)
     {
         auto temp{ ans };
-        for (auto& set : temp) set.push_back(e);
-        ans.insert(ans.end(), temp.begin(), temp.end());
-
+        for (auto& set : temp) set.emplace_back(e);
+        ans.insert(ans.begin(), temp.begin(), temp.end());
     }
-    return ans;
-}
-
-void solution(std::vector<int>& nums, std::vector<std::vector<int>>& ans, std::vector<int>& curr, int index)
-{
-    if (index > nums.size()) return;
-    ans.push_back(curr);
-    for (int i = index; i < nums.size(); i++)
-    {
-        if (find(curr.begin(), curr.end(), nums[i]) == curr.end())
-        {
-            curr.push_back(nums[i]);
-            solution(nums, ans, curr, i);
-            curr.pop_back();
-        }
-    }
-    return;
-}
-
-std::vector<std::vector<int>> subsets2(std::vector<int>& nums)
-{
-    std::vector<std::vector<int>> ans;
-    std::vector<int> curr;
-    solution(nums, ans, curr, 0);
     return ans;
 }
 
@@ -59,9 +35,6 @@ void printSet(std::vector<std::vector<int>> set)
 int main()
 {
     std::vector<int> ex1{ 1, 2, 3 };
-    std::vector<std::vector<int>> ans1{ subsets1(ex1) };
+    std::vector<std::vector<int>> ans1{ subsets(ex1) };
     printSet(ans1);
-
-    std::vector<std::vector<int>> ans2{ subsets2(ex1) };
-    printSet(ans2);
 }
