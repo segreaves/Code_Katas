610910746e6213c72657d715ad8f1583d787764e
Simon Greaves <35945230+segreaves@users.noreply.github.com>
2026-05-06 11:28:13 -0400

    MissingNumber (XOR method)
    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
    
    Example 1:
    Input: nums = [3,0,1]
    Output: 2
    
    Explanation:
    n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
    
    Example 2:
    Input: nums = [0,1]
    Output: 2
    Explanation:
    n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
    
    Example 3:
    Input: nums = [9,6,4,2,3,5,7,0,1]
    Output: 8
    Explanation:
    n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.

--- MissingNumber.cpp
+++ MissingNumber.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-int missingNumber(std::vector<int>& nums)
+int missingNumber_math(std::vector<int>& nums)
 {
     // For nums = [0, 1, ..., n] the sum of nums should be n(n-1)/2.
     // If there is a missing number, it must be equal to n(n-1)/2 - sum(nums)
@@ -12,17 +12,30 @@ int missingNumber(std::vector<int>& nums)
     return (n * (n - 1) / 2) - tot;
 }
 
+int missingNumber(std::vector<int>& nums)
+{
+    const auto n{ nums.size() };
+    int full{ 0 };
+    int miss{ 0 };
+    for (int i = 0; i < n + 1; ++i)
+    {
+        if (i < n) miss ^= nums[i];
+        full ^= i;
+    }
+    return full ^ miss;
+}
+
 int main()
 {
     std::vector<int> ex1{3, 0, 1};
     int ans1{missingNumber(ex1)};
-    std::cout << "Should be 2: " << ans1 << std::endl;
+    std::cout << "Should be 2: " << ans1 << "\n";
 
     std::vector<int> ex2{0, 1};
     int ans2{missingNumber(ex2)};
-    std::cout << "Should be 2: " << ans2 << std::endl;
+    std::cout << "Should be 2: " << ans2 << "\n";
 
     std::vector<int> ex3{9, 6, 4, 2, 3, 5, 7, 0, 1};
     int ans3{missingNumber(ex3)};
-    std::cout << "Should be 8: " << ans3 << std::endl;
+    std::cout << "Should be 8: " << ans3 << "\n";
 }
